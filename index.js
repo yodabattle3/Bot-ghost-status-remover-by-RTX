758b79d2d6e825e56eef4af4fc028fa2c4a96dc9
INeedOptiFabric <126305706+INeedOptiFabric@users.noreply.github.com>
2024-01-22 21:49:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = ["dicord.gg/ValoVault"];
 
 
 let currentIndex = 0;
